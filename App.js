7b767042a78a92e98489a1150e026e61b8929c01
Ravi Bharti <ravics1718@gmail.com>
2021-09-16 01:26:38 +0530

    first commit 🌈

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.logo}>Doodllr</Text>
+      <Text>Welcome to the app</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +19,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    fontSize: 36,
+    color: "#4d148c",
+    fontWeight: "bold",
+  }
 });
