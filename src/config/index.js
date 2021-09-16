fa56c34fbc5d1f7ba094f923ff6d1720ac4010ba
Ravi Bharti <ravics1718@gmail.com>
2021-09-17 00:37:23 +0530

    create folder structure & prettier config

---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+// Light theme color
+export const COLORS = {
+  primary: "#F0F6FE",
+  secondary: "#f76f8c",
+  success: "#1cc1c2",
+  lightGrey: "#8aa2c9",
+  light100: "#f0f6fe",
+  light200: "#e0e9f9",
+};
+
+// font configuration
+export const FONTS = {
+  bold: "bold",
+  normal: "normal",
+};
